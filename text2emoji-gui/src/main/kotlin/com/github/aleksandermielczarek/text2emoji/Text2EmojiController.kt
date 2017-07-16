853d9cc4bd18dd2f5919a29d1c57c5a4005a0047
Aleksander Mielczarek <alekmiel91@gmail.com>
2017-07-16 14:59:35 +0200

    Add mvvm

--- text2emoji-gui/src/main/kotlin/com/github/aleksandermielczarek/text2emoji/Text2EmojiController.kt
+++ text2emoji-gui/src/main/kotlin/com/github/aleksandermielczarek/text2emoji/Text2EmojiController.kt
@@ -1,122 +1,69 @@
 package com.github.aleksandermielczarek.text2emoji
 
-import com.github.thomasnield.rxkotlinfx.textValues
-import com.github.thomasnield.rxkotlinfx.toBinding
-import com.github.thomasnield.rxkotlinfx.toObservable
-import io.reactivex.Observable
-import io.reactivex.rxkotlin.Observables
-import javafx.beans.property.Property
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.scene.control.*
-import javafx.scene.input.Clipboard
-import javafx.scene.input.ClipboardContent
 
 /**
  * Created by Aleksander Mielczarek on 14.07.2017.
  */
 class Text2EmojiController {
 
-    companion object {
-        val MAX_WIDTH = 100
-    }
+    @FXML
+    lateinit var text: TextField
 
     @FXML
-    private lateinit var text: TextField
+    lateinit var textEmoji: TextField
 
     @FXML
-    private lateinit var textEmoji: TextField
+    lateinit var emptyEmoji: TextField
 
     @FXML
-    private lateinit var emptyEmoji: TextField
+    lateinit var separatorEnabled: CheckBox
 
     @FXML
-    private lateinit var separator: TextField
+    lateinit var separator: TextField
 
     @FXML
-    private lateinit var separatorEnabled: CheckBox
+    lateinit var widthEnabled: CheckBox
 
     @FXML
-    private lateinit var widthEnabled: CheckBox
+    lateinit var width: Spinner<Int>
 
     @FXML
-    private lateinit var width: Spinner<Int>
+    lateinit var emojis: TextArea
 
-    @FXML
-    private lateinit var emojis: TextArea
-
-    private val text2Emoji = Text2Emoji()
+    private val viewModel = Text2EmojiViewModel()
 
     @FXML
     fun initialize() {
         initSpinner()
         bindWidgets()
-        observeText2emoji()
+        viewModel.observeText2emoji()
     }
 
     @FXML
     fun copyToClipboard(event: ActionEvent) {
-        val clipboard = Clipboard.getSystemClipboard()
-        val clipboardContent = ClipboardContent()
-        clipboardContent.putString(emojis.text)
-        clipboard.setContent(clipboardContent)
+        viewModel.copyToClipboard()
     }
 
     private fun initSpinner() {
-        val spinnerValueFactory = SpinnerValueFactory.IntegerSpinnerValueFactory(Symbol.width, MAX_WIDTH)
+        val spinnerValueFactory = SpinnerValueFactory.IntegerSpinnerValueFactory(Symbol.width, Text2EmojiViewModel.MAX_WIDTH)
         width.valueFactory = spinnerValueFactory
     }
 
     private fun bindWidgets() {
         width.disableProperty().bind(widthEnabled.selectedProperty().not())
         separator.disableProperty().bind(separatorEnabled.selectedProperty().not())
-    }
 
-    private fun observeText2emoji() {
-        Observables.combineLatest(text.textValues,
-                textEmoji.textValues,
-                emptyEmoji.textValues,
-                separatorEnabled.values(),
-                separator.textValues,
-                widthEnabled.values(),
-                width.values(),
-                this::text2emoji)
-                .bind(emojis.textProperty())
-    }
-
-    private fun text2emoji(text: String, textEmoji: String, emptyEmoji: String, separatorEnabled: Boolean, separator: String, widthEnabled: Boolean, width: Int): String {
-        val currentWidth = resolveWidth(width, widthEnabled)
-        val currentSeparator = resolveSeparator(separator, separatorEnabled)
-        val emojis = text2Emoji.text2emoji(text, textEmoji, emptyEmoji, currentSeparator, currentWidth)
-        return emojis.joinToString(System.lineSeparator())
-    }
-
-    private fun resolveSeparator(separator: String, separatorEnabled: Boolean): String {
-        if (separatorEnabled) {
-            return separator
-        } else {
-            return ""
-        }
-    }
-
-    private fun resolveWidth(width: Int, widthEnabled: Boolean): Int {
-        if (widthEnabled) {
-            return width
-        } else {
-            return Text2Emoji.WIDTH_NO_LIMIT
-        }
-    }
-
-    private fun CheckBox.values(): Observable<Boolean> {
-        return selectedProperty().toObservable()
-    }
-
-    private fun <T> Spinner<T>.values(): Observable<T> {
-        return valueProperty().toObservable()
-    }
-
-    private fun <T> Observable<T>.bind(property: Property<T>) {
-        property.bind(this.toBinding())
+        viewModel.text.bind(text.textProperty())
+        viewModel.textEmoji.bind(textEmoji.textProperty())
+        viewModel.emptyEmoji.bind(emptyEmoji.textProperty())
+        viewModel.separatorEnabled.bind(separatorEnabled.selectedProperty())
+        viewModel.separator.bind(separator.textProperty())
+        viewModel.widthEnabled.bind(widthEnabled.selectedProperty())
+        viewModel.width.bind(width.valueProperty())
+        emojis.textProperty().bind(viewModel.emojis)
     }
 
 }
---
+++ text2emoji-gui/src/main/kotlin/com/github/aleksandermielczarek/text2emoji/Text2EmojiViewModel.kt
@@ -0,0 +1,74 @@
+package com.github.aleksandermielczarek.text2emoji
+
+import com.github.thomasnield.rxkotlinfx.toBinding
+import com.github.thomasnield.rxkotlinfx.toObservable
+import io.reactivex.rxkotlin.Observables
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.scene.input.Clipboard
+import javafx.scene.input.ClipboardContent
+
+/**
+ * Created by Aleksander Mielczarek on 16.07.2017.
+ */
+class Text2EmojiViewModel {
+
+    companion object {
+        val MAX_WIDTH = 100
+    }
+
+    val text2Emoji = Text2Emoji()
+
+    val text = SimpleStringProperty()
+    val textEmoji = SimpleStringProperty()
+    val emptyEmoji = SimpleStringProperty()
+    val separatorEnabled = SimpleBooleanProperty()
+    val separator = SimpleStringProperty()
+    val widthEnabled = SimpleBooleanProperty()
+    val width = SimpleIntegerProperty()
+    val emojis = SimpleStringProperty()
+
+    fun copyToClipboard() {
+        val clipboard = Clipboard.getSystemClipboard()
+        val clipboardContent = ClipboardContent()
+        clipboardContent.putString(emojis.value)
+        clipboard.setContent(clipboardContent)
+    }
+
+    fun observeText2emoji() {
+        emojis.bind(Observables.combineLatest(text.toObservable(),
+                textEmoji.toObservable(),
+                emptyEmoji.toObservable(),
+                separatorEnabled.toObservable(),
+                separator.toObservable(),
+                widthEnabled.toObservable(),
+                width.toObservable().map { it.toInt() },
+                this::text2emoji)
+                .toBinding())
+    }
+
+    private fun text2emoji(text: String, textEmoji: String, emptyEmoji: String, separatorEnabled: Boolean, separator: String, widthEnabled: Boolean, width: Int): String {
+        val currentWidth = resolveWidth(width, widthEnabled)
+        val currentSeparator = resolveSeparator(separator, separatorEnabled)
+        val emojis = text2Emoji.text2emoji(text, textEmoji, emptyEmoji, currentSeparator, currentWidth)
+        return emojis.joinToString(System.lineSeparator())
+    }
+
+    private fun resolveSeparator(separator: String, separatorEnabled: Boolean): String {
+        if (separatorEnabled) {
+            return separator
+        } else {
+            return ""
+        }
+    }
+
+    private fun resolveWidth(width: Int, widthEnabled: Boolean): Int {
+        if (widthEnabled) {
+            return width
+        } else {
+            return Text2Emoji.WIDTH_NO_LIMIT
+        }
+    }
+
+}
